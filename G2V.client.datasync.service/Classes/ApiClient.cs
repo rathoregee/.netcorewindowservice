3aa7c58fde7577750f7ba3abe4056b8cfaf7137e
kamran bashir <rathore_gee@hotmail.com>
2023-02-10 13:01:05 -0700

    added api client

---
+++ G2V.client.datasync.service/Classes/ApiClient.cs
@@ -0,0 +1,106 @@
+﻿using G2V.client.datasync.service.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G2V.client.datasync.service.Classes
+{
+    public class ApiClient : IApiClient
+    {
+        private IHttpClientFactory _factory;
+        private HttpClient? _client = null;
+        private string _clientName;
+
+        public ApiClient(IHttpClientFactory httpClientFactory, string ClientName)
+        {
+            _factory = httpClientFactory;
+            _clientName = ClientName;
+        }
+
+        #region Generic, Async, static HTTP functions for GET, POST, PUT, and DELETE             
+
+        public async Task<T> GetAsync<T>(string url)
+        {
+            
+            try
+            {
+                T data;
+                _client = _factory.CreateClient(_clientName);
+                using HttpResponseMessage response = await _client.GetAsync(url);
+                using HttpContent content = response.Content;
+                string d = await content.ReadAsStringAsync();
+                if (d != null)
+                {
+                    data = JsonConvert.DeserializeObject<T>(d);
+                    return (T)data;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            Object o = new Object();
+            return (T)o;
+        }
+
+        public async Task<T> PostAsync<T>(string url, HttpContent contentPost)
+        {
+            T data;
+            _client = _factory.CreateClient(_clientName);
+            using (HttpResponseMessage response = await _client.PostAsync(url, contentPost))
+            using (HttpContent content = response.Content)
+            {
+                string d = await content.ReadAsStringAsync();
+                if (d != null)
+                {
+                    data = JsonConvert.DeserializeObject<T>(d);
+                    return (T)data;
+                }
+            }
+            Object o = new Object();
+            return (T)o;
+        }
+
+        public async Task<T> PutAsync<T>(string url, HttpContent contentPut)
+        {
+            T data;
+            _client = _factory.CreateClient(_clientName);
+
+            using (HttpResponseMessage response = await _client.PutAsync(url, contentPut))
+            using (HttpContent content = response.Content)
+            {
+                string d = await content.ReadAsStringAsync();
+                if (d != null)
+                {
+                    data = JsonConvert.DeserializeObject<T>(d);
+                    return (T)data;
+                }
+            }
+            Object o = new Object();
+            return (T)o;
+        }
+
+        public async Task<T> DeleteAsync<T>(string url)
+        {
+            T newT;
+            _client = _factory.CreateClient(_clientName);
+
+            using (HttpResponseMessage response = await _client.DeleteAsync(url))
+            using (HttpContent content = response.Content)
+            {
+                string data = await content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    newT = JsonConvert.DeserializeObject<T>(data);
+                    return newT;
+                }
+            }
+            Object o = new Object();
+            return (T)o;
+        }
+        #endregion
+    }
+}
---
+++ G2V.client.datasync.service/Interfaces/IApiClient.cs
@@ -0,0 +1,12 @@
+﻿
+namespace G2V.client.datasync.service.Interfaces
+
+{
+    public interface IApiClient
+    {
+        Task<T> DeleteAsync<T>(string url);
+        Task<T> GetAsync<T>(string url);
+        Task<T> PostAsync<T>(string url, HttpContent contentPost);
+        Task<T> PutAsync<T>(string url, HttpContent contentPut);
+    }
+}
